535a5074bfff540050770461474f1da9e5fa82c4
Anand Chaudhary <achaudharyskn@gmail.com>
2026-02-11 16:46:13 +0530

    feat:build a simple pokemone game

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 
+const colorTypes = {
+   grass:"green",
+   fire:"orange",
+   water:"blue",
+   bug:"greeen"
+}
 export default function App() {
+  const[pokemons,setPokemon] = useState([]);
+  console.log(JSON.stringify(pokemons[0],null,2))
+
+
+  useEffect(()=>{
+     //fetch pokemon
+     fetchPokemons() 
+  },[]);
+
+  async function fetchPokemons(){
+    try{
+        console.log("Hii hows")
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon/?limit=5"
+        );
+        const data = await response.json();
+
+        const detailPokemons = await Promise.all(
+          data.results.map(async(pokemon)=>{
+            const res = await fetch(pokemon.url);
+            const details = await res.json();
+
+            return{
+              name : pokemon.name,
+              image:details.sprites.front_default,
+              imageBack : details.sprites.back_default,
+              types:details.types
+            }
+          })
+        )
+        console.log(detailPokemons)
+        setPokemon(detailPokemons)
+    }catch(e){
+
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open  Hellp Hos sdfasfdworking on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView 
+      contentContainerStyle = {{
+        gap:16,
+        padding:16
+      }}
+    >
+      {pokemons.map((pokemon)=>(
+        <View key={pokemon.name} style={{
+          backgroundColor : colorTypes[pokemon.types[0].type.name],
+          padding : 20
+        }}>
+          <Text style={style.name}>{pokemon.name}</Text>
+          <Text style={style.type}>{pokemon.types[0].type.name}</Text>
+
+          <View
+            style = {{
+              flexDirection:"row"
+            }} 
+          >
+            <Image
+              source={{uri:pokemon.image}}
+              style = {{width:100,height:100}}
+            >
+            </Image>
+            <Image
+              source={{uri:pokemon.imageBack}}
+              style = {{width:150,height:150}}
+            >
+          </Image>
+          </View>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#6b5656',
-    alignItems: 'center',
-    justifyContent: 'center',
+
+
+
+const style = StyleSheet.create({
+  name:{
+    fontSize : 28,
+    fontWeight:"bold"
   },
-});
+  type:{
+    fontSize:20,
+    fontWeight:"bold",
+    color:"gray"
+
+  }
+})
